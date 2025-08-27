eb69bd3b786600dd9c94acaeb7a228426a2ec54a
unknown <vladimirovicp70@gmail.com>
2025-08-28 01:47:39 +0400

    feat: add script generate table

---
+++ js/data.js
@@ -0,0 +1,67 @@
+export const tablesData = [
+  {
+    caption: "mate",
+    basePath: "./img/alt-workstation-10.4/mate",
+    type: "png",
+    folder: "places",
+    sizes: [16, 22, 32, 48, 256],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" },
+      { iconName: "OBJECT_CATEGORY_CONTAINER", name: "folder" },
+      { iconName: "OBJECT_CATEGORY_OU", name: "folder-documents" },
+      { iconName: "OBJECT_CATEGORY_GROUP", name: "system-users" },
+      { iconName: "OBJECT_CATEGORY_PERSON", name: "avatar-default" },
+      { iconName: "OBJECT_CATEGORY_PERSON", name: "avatar-default-symbolic", custom: true }
+    ]
+  },
+  {
+    caption: "Papirus",
+    basePath: "./img/alt-workstation-10.4/Papirus",
+    type: "svg",
+    folder: "devices",
+    sizes: [16, 22, 32, 48, 64],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" }
+    ]
+  },
+  {
+    caption: "ContrastHigh",
+    basePath: "./img/alt-workstation-10.4/ContrastHigh",
+    type: "png",
+    folder: "places",
+    sizes: [16, 22, 24, 32, 48, 256, "scalable"],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" }
+    ]
+  },
+  {
+    caption: "Adwaita",
+    basePath: "./img/alt-workstation-10.4/Adwaita",
+    type: "png",
+    folder: "places",
+    sizes: [16, 22, 24, 32, 48, 256],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" }
+    ]
+  },
+  {
+    caption: "gnome",
+    basePath: "./img/alt-workstation-10.4/gnome",
+    type: "png",
+    folder: "places",
+    sizes: [16, 22, 24, 32, 48, 256],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" }
+    ]
+  },
+  {
+    caption: "Papirus-Dark",
+    basePath: "./img/alt-workstation-10.4/Papirus-Dark",
+    type: "svg",
+    folder: "devices",
+    sizes: [16, 22, 24, 32, 48, 64, 96, 128],
+    rows: [
+      { iconName: "OBJECT_CATEGORY_DOMAIN_DNS", name: "network-server" }
+    ]
+  }
+];
---
+++ js/table.js
@@ -0,0 +1,87 @@
+import { tablesData } from "./data.js";
+
+export function createTable(tableData) {
+  const table = document.createElement("table");
+
+  // caption
+  const caption = document.createElement("caption");
+  caption.textContent = tableData.caption;
+  table.appendChild(caption);
+
+  // thead
+  const thead = document.createElement("thead");
+  const headerRow = document.createElement("tr");
+  const headers = ["Icon Name", "Name", ...tableData.sizes.map(s => `${s}x${s}`)];
+
+  headers.forEach(text => {
+    const th = document.createElement("th");
+    th.scope = "col";
+    th.textContent = text;
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // tbody
+  const tbody = document.createElement("tbody");
+
+  tableData.rows.forEach(row => {
+    const tr = document.createElement("tr");
+
+    // iconName
+    const tdIcon = document.createElement("td");
+    tdIcon.textContent = row.iconName;
+    tr.appendChild(tdIcon);
+
+    // name
+    const tdName = document.createElement("td");
+    tdName.scope = "row";
+    tdName.textContent = row.name;
+    tr.appendChild(tdName);
+
+    // sizes
+    tableData.sizes.forEach(size => {
+      const td = document.createElement("td");
+      td.scope = "col";
+
+      if (!row.custom) {
+        const img = document.createElement("img");
+        if(size === 'scalable'){
+            img.src = `${tableData.basePath}/${size}/${tableData.folder}/${row.name}.svg`;
+        } else{
+            img.src = `${tableData.basePath}/${size}x${size}/${tableData.folder}/${row.name}.${tableData.type}`;
+        }
+        
+        img.alt = row.name;
+        td.appendChild(img);
+      } else if (row.name === "avatar-default-symbolic") {
+        // спец. случай
+            if(size === 256){
+                const img = document.createElement("img");
+                img.src = `${tableData.basePath}/scalable/status/${row.name}.svg`;
+                img.width = 256;
+                img.height = 256;
+                img.alt = row.name;
+                td.appendChild(img);
+            } 
+        }
+      
+
+      tr.appendChild(td);
+    });
+
+    tbody.appendChild(tr);
+  });
+
+  table.appendChild(tbody);
+
+  return table;
+}
+
+export function renderAllTables(containerId) {
+  const container = document.getElementById(containerId);
+  tablesData.forEach(t => {
+    const table = createTable(t);
+    container.appendChild(table);
+  });
+}
